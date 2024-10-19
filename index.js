912899deed36c2e0c2d23c731f4a915904128540
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-10-19 19:04:29 +0000

    Ehyegtst

--- index.js
+++ index.js
@@ -7,19 +7,18 @@ const path = require("path");
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-function handlePostback(event, pageAccessToken) {
+const handlePostback = (event, pageAccessToken) => {
   const senderId = event.sender.id;
   const payload = event.postback.payload;
 
   sendMessage(senderId, { text: `You sent a postback with payload: ${payload}` }, pageAccessToken);
-}
+};
 
-
-function sendMessage(senderId, message, pageAccessToken) {
+const sendMessage = (senderId, message, pageAccessToken) => {
   if (!message || (!message.text && !message.attachment)) {
     return;
   }
@@ -44,12 +43,12 @@ function sendMessage(senderId, message, pageAccessToken) {
     json: payload,
   }, (error, response, body) => {
     if (error) {
-    } else if (response.body.error) {     
-    } else {
+      console.error('Error sending message:', error);
+    } else if (response.body.error) {
+      console.error('Error response from API:', response.body.error);
     }
   });
-}
-
+};
 
 const commands = new Map();
 
@@ -59,7 +58,7 @@ for (const file of commandFiles) {
   commands.set(command.name, command);
 }
 
-async function handleMessage(event, pageAccessToken) {
+const handleMessage = (event, pageAccessToken) => {
   const senderId = event.sender.id;
 
   if (!event.message || !event.message.text) {
@@ -68,25 +67,23 @@ async function handleMessage(event, pageAccessToken) {
   }
 
   const messageText = event.message.text;
-
   const args = messageText.split(' ');
   const commandName = args.shift().toLowerCase();
 
   if (commands.has(commandName)) {
     const command = commands.get(commandName);
-    try {
-      await command.execute(senderId, args, pageAccessToken, sendMessage);
-    } catch (error) {
-      sendMessage(senderId, { text: 'There was an error executing that command.' }, pageAccessToken);
-    }
+    command.execute(senderId, args, pageAccessToken, sendMessage)
+      .catch(error => {
+        sendMessage(senderId, { text: 'There was an error executing that command.' }, pageAccessToken);
+        console.error('Error executing command:', error);
+      });
   } else {
     sendMessage(senderId, { text: `Invalid command '${commandName}' please use help to see the list of available commands` }, pageAccessToken);
   }
-}
+};
 
 const VERIFY_TOKEN = 'shipazu';
-
-const PAGE_ACCESS_TOKEN = fs.readFileSync('token.txt', 'utf8').trim();
+const PAGE_ACCESS_TOKEN = "EAANTypknxAUBO3dfC9InqVvZBbLkWcNm7Qf2p6PdO0zb7eDIp05e4WfeIGWtAqVXNDk5amsJdmuV6ga9ZCHcnIKXjyVsVMZCsOds11eHdh1uAPEjZAZAfdNNMFn6tOQsoRhbZAnRmV3qPHH9pteYxeFiS9AJXt4NGnDZAZCGNZATGkWug4IIEfkKWlMEWkUznZAGBX";
 
 app.get('/webhook', (req, res) => {
   const mode = req.query['hub.mode'];
