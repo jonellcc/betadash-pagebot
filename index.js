40f5ba60bb8fbe2500bd11ab572a90501c89dd01
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-10-19 20:23:14 +0000

    Hdfv

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.post('/webhook', (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
