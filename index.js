2821ff93817944baa029ee4dd43cc7d0ece9d348
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-10-21 09:20:51 +0000

    Jshshs

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const commandList = [];
 const descriptions = [];
 const commands = new Map();
 
-app.get('/webhook', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get('/webhook', (req, res) => {
