da145381fcde33427291537122b7eb822bd7e568
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-10-26 05:32:04 +0000

    Jzhzhz

--- commands/id.js
+++ commands/id.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "id",
   description: "Check your user ID while on a page bot",
-  execute(senderId, event, sendMessage, pageAccessToken) {
+  async execute(senderId, event, sendMessage, pageAccessToken) {
 const uid = `Your ID: ${senderId}`;
     sendMessage(senderId, { text: uid}, pageAccessToken);
    return;
