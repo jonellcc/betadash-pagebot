63f49560a6730c2cbbb22cf821e4a7967dc55e66
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-10-29 11:10:32 +0000

    Ffwgev

---
+++ commands/music.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+const headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+  'Content-Type': 'application/json'
+};
+
+module.exports = {
+  name: 'music',
+  description: 'Get an MP3 download link for a song from YouTube',
+  author: 'Cliff',
+  async execute(senderId, args, pageAccessToken, sendMessage) {
+    const query = args.join(' ');
+    if (!query) {
+      sendMessage(senderId, { text: 'Please provide the name of the music you want to search' }, pageAccessToken);
+      return;
+    }
+
+    try {
+      sendMessage(senderId, { text: `🔍 | Searching music: ${query}` }, pageAccessToken);
+      const apiUrl = `https://kaiz-audiomp3.vercel.app/ytmp3?q=${encodeURIComponent(query)}`;
+      const response = await axios.get(apiUrl, { headers });
+
+      const { mp3Link, title, duration, thumbnail } = response.data;
+
+const kupal = `💽 Now playing\n\n𝗧𝗶𝘁𝗹𝗲: ${title}\n𝗗𝘂𝗿𝗮𝘁𝗶𝗼𝗻: ${duration}\n\nSending music wait a seconds...`;
+sendMessage(senderId, {text: kupal}, pageAccessToken);
+    sendMessage(senderId, {
+          attachment: {
+            type: 'image',
+            payload: {
+              url: thumbnail,
+              is_reusable: true
+            }
+          }
+        }, pageAccessToken);
+
+      if (mp3Link) {
+        sendMessage(senderId, {
+          attachment: {
+            type: 'audio',
+            payload: {
+              url: mp3Link,
+              is_reusable: true
+            }
+          }
+        }, pageAccessToken);
+      } else {
+        sendMessage(senderId, { text: `Sorry, no download link found for the ${query}`}, pageAccessToken);
+      }
+    } catch (error) {
+      sendMessage(senderId, { text: 'Sorry, there was an error processing your request.' }, pageAccessToken);
+    }
+  }
+};
