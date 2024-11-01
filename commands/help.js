622b4772c9ad2e5198e802a5d7ff6d2ed7376b5d
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-11-01 21:42:28 +0000

    Nhfbhd

--- commands/help.js
+++ commands/help.js
@@ -107,7 +107,7 @@ module.exports = {
       return `│ ✧ ${command.name}`;
     });
 
-const huys = "╭─❍「 𝗛𝗜𝗗𝗘𝗡 𝗙𝗘𝗔𝗧𝗨𝗥𝗘𝗦 」\n│ ● Autodownloader - Insta, Tiktok, Facebook, Youtube.\n│ ● imgur - Reply to an image to upload in imgur.\n│ ● prompt - Reply image to get the exact prompt.\n│ ● tinyurl - Reply to image to shorter url.\n│ ● gdrive - Reply to image to upload in gdrive.\n│ ● removebg - Reply a photo to Remove background image.\n│ ● remini - Reply a photo to Enhancing image.";
+const huys = "\t\t「 𝗛𝗜𝗗𝗘𝗡 𝗙𝗘𝗔𝗧𝗨𝗥𝗘𝗦 」\n● Autodownloader - Insta, Tiktok, Facebook, Youtube.\n● imgur - Reply to an image to upload in imgur.\n● prompt - Reply image to get the exact prompt.\n│ ● tinyurl - Reply to image to shorter url.\n● gdrive - Reply to image to upload in gdrive.\n● removebg - Reply a photo to Remove background image.\n● remini - Reply a photo to Enhancing image.";
 
     const helpMessage = `🛠️ ${formatFont("Available Commands")}\n\n╭─❍「 ${formatFont("NO PREFIX")} 」\n${commandsList.join('\n')}\n╰───────────◊\n\n${huys}\n\n» 𝗣𝗮𝗴𝗲: <${pageNumber}/${Math.ceil(totalCommands / commandsPerPage)}>\n» 𝗧𝗼𝘁𝗮𝗹 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀: [ ${totalCommands} ]\n» 𝗥𝗔𝗡𝗗𝗢𝗠 𝗙𝗔𝗖𝗧: ${randomQuote}`;
 
