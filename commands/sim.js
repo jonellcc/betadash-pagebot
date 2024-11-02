743dc355c7aea439c87520eefa578fa57b6279da
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-11-02 16:40:34 +0000

    Jjfnhfnhf

--- commands/sim.js
+++ commands/sim.js
@@ -6,15 +6,20 @@ module.exports = {
   author: 'markdevs (rest api)',
   async execute(senderId, args, pageAccessToken, sendMessage, splitMessageIntoChunks) {
     const prompt = args.join(' ');
-if (!prompt) {
-          sendMessage(senderId, { text: 'please provide a text to talk simsimifun' }, pageAccessToken);
-        return;
+
+    if (!prompt) {
+      sendMessage(senderId, { text: 'Please provide a text to talk to simsimifun' }, pageAccessToken);
+      return;
     }
 
     try {
       const apiUrl = `https://apis-markdevs69v2.onrender.com/api/simv2/get/${prompt}`;
       const response = await axios.get(apiUrl);
       const text = response.data.reply;
+
+      // Send the API response text back to the user
+      sendMessage(senderId, { text }, pageAccessToken);
+
     } catch (error) {
       sendMessage(senderId, { text: 'Sorry, there was an error processing your request.' }, pageAccessToken);
     }
