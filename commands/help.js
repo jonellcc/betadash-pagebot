abd58ebd94f74b8da11a72d7be28cc716582a8c9
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-11-06 16:35:03 +0000

    Fgsfvs

--- commands/help.js
+++ commands/help.js
@@ -111,8 +111,7 @@ module.exports = {
 
 const huys = "\t「 𝗛𝗜𝗗𝗘𝗡 𝗙𝗘𝗔𝗧𝗨𝗥𝗘𝗦 」\n● Autodownloader - Insta, Tiktok, Facebook, Youtube, Capcut, SoundCloud, Spotify.\n● imgur - Reply to an image to upload in imgur.\n● prompt - Reply image to get the exact prompt.\n● tinyurl - Reply to image to shorter url.\n● removebg - Reply a photo to Remove background image.\n● remini - Reply a photo to Enhancing image.";
 
-    const helpMessage = `🛠️ ${formatFont("Available Commands")}\n\n╭─❍「 ${formatFont("NO PREFIX")} 」\n${commandsList.join('\n')}\n╰───────────◊\n\n${huys}\n\n» 𝗣𝗮𝗴𝗲: <${pageNumber}/${Math.ceil(totalCommands / commandsPerPage)}>\n» 𝗚𝘂𝗶𝗱𝗲: "Type help 1 or any pages
-or help <command_name> to view detailed usage instructions for a specific command"\n» 𝗧𝗼𝘁𝗮𝗹 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀: [ ${totalCommands} ]\n» 𝗥𝗔𝗡𝗗𝗢𝗠 𝗙𝗔𝗖𝗧: ${randomQuote}`;
+    const helpMessage = `🛠️ ${formatFont("Available Commands")}\n\n╭─❍「 ${formatFont("NO PREFIX")} 」\n${commandsList.join('\n')}\n╰───────────◊\n\n${huys}\n\n» 𝗣𝗮𝗴𝗲: <${pageNumber}/${Math.ceil(totalCommands / commandsPerPage)}>\n» 𝗚𝘂𝗶𝗱𝗲: "Type help 1 or any pages number or help <command_name> to view detailed usage instructions for a specific command"\n» 𝗧𝗼𝘁𝗮𝗹 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀: [ ${totalCommands} ]\n» 𝗥𝗔𝗡𝗗𝗢𝗠 𝗙𝗔𝗖𝗧: ${randomQuote}`;
 
 const kupal = {
       text: helpMessage,
