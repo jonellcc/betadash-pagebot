df54468a43c4e2d9ac4602c46e0c66dc58d7879d
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-12-01 16:08:58 +0000

    Figg

--- commands/horny.js
+++ commands/horny.js
@@ -10,27 +10,15 @@ module.exports = {
       "https://i.imgur.com/0azRXWN.mp4",
       "https://i.imgur.com/2YO1UIV.mp4",
       "https://i.imgur.com/ht6pqg3.mp4",
-      "https://i.imgur.com/2EDrkgt.mp4",
       "https://i.imgur.com/pFB6VnE.mp4",
       "https://i.imgur.com/jruTcFx.mp4",
-      "https://i.imgur.com/vDHfKxh.mp4",
       "https://i.imgur.com/bwihQeY.mp4",
-      "https://i.imgur.com/JMuALc1.mp4",
       "https://i.imgur.com/8lIZa5G.mp4",
       "https://i.imgur.com/LR9ooyJ.mp4",
       "https://i.imgur.com/Viv4X8C.mp4",
       "https://i.imgur.com/cspl74z.mp4",
-      "https://i.imgur.com/SzPPdqW.mp4",
       "https://i.imgur.com/QZRP8Sn.mp4",
       "https://i.imgur.com/pCbdZYY.mp4",
-      "https://i.imgur.com/j6CrJxW.mp4",
-      "https://i.imgur.com/uthREbe.mp4",
-      "https://i.imgur.com/v4mLGte.mp4",
-      "https://i.imgur.com/DJylTiy.mp4",
-      "https://i.imgur.com/ee8fHna.mp4",
-      "https://i.imgur.com/VffzOwS.mp4",
-      "https://i.imgur.com/ci5nztg.mp4",
-      "https://i.imgur.com/qHPeKDV.mp4",
       "https://i.imgur.com/Rkl5UmH.mp4",
       "https://i.imgur.com/IGXINCB.mp4",
       "https://i.imgur.com/qxPR4i6.mp4",
@@ -73,23 +61,19 @@ module.exports = {
       "https://i.imgur.com/bim2U8C.mp4",
       "https://i.imgur.com/YzlGSlm.mp4",
       "https://i.imgur.com/HZpxU7h.mp4",
-      "https://i.imgur.com/exTO3J4.mp4",
-      "https://i.imgur.com/Xf6HVcA.mp4",
       "https://i.imgur.com/9iOci5S.mp4",
       "https://i.imgur.com/6w5tnvs.mp4",
       "https://i.imgur.com/1L0DMtl.mp4",
-      "https://i.imgur.com/7wcQ8eW.mp4",
       "https://i.imgur.com/3MBTpM8.mp4",
       "https://i.imgur.com/8h1Vgum.mp4",
-      "https://i.imgur.com/CTcsUZk.mp4",
       "https://i.imgur.com/e505Ko2.mp4",
       "https://i.imgur.com/3umJ6NL.mp4"
-    ];
+]; 
 
-    try {
-      const randomIndex = Math.floor(Math.random() * links.length);
-      const videoUrl = links[randomIndex];
+    const randomIndex = Math.floor(Math.random() * links.length);
+    const videoUrl = links[randomIndex];
 
+    if (videoUrl) {
       sendMessage(senderId, {
         attachment: {
           type: 'video',
@@ -121,8 +105,8 @@ module.exports = {
           }
         ]
       }, pageAccessToken);
-    } catch (error) {
-      sendMessage(senderId, { text: 'Sorry, there was an error processing your request.' }, pageAccessToken);
+    } else {
+      sendMessage(senderId, { text: 'Sorry, no video found.' }, pageAccessToken);
     }
   }
 };
