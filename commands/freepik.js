9f94297dcf9fd9129ad9df070d0d1541071462c9
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2025-01-23 19:42:48 +0800

    Update freepik.js

--- commands/freepik.js
+++ commands/freepik.js
@@ -2,44 +2,34 @@ const axios = require("axios");
 
 module.exports = {
   name: 'freepik',
-  description: 'Generate images from Freepik',
-  usage: 'freepik <search>',
+  description: 'Generate images from freepik',
+  usage: 'freepik <prompt>',
   author: 'Cliff',
   async execute(senderId, args, pageAccessToken, sendMessage) {
     if (!args || !Array.isArray(args) || args.length === 0) {
-      await sendMessage(senderId, { text: 'Please provide a prompt.' }, pageAccessToken);
+      await sendMessage(senderId, { text: 'Please provide a prompt to generate an image' }, pageAccessToken);
       return;
     }
 
     const prompt = args.join(' ');
 
     try {
-      await sendMessage(senderId, { text: "֎ | Generating, please wait...." }, pageAccessToken);
-
+      await sendMessage(senderId, { text: "֎ | Generating Please Wait...."}, pageAccessToken);
       const apiUrl = `https://betadash-api-swordslush.vercel.app/freepik?search=${encodeURIComponent(prompt)}`;
+
       const response = await axios.get(apiUrl);
-      const images = response.data.images.slice(0, 5);
+      const images = response.data.images;
 
-      const attachments = images.map((url) => ({
-        media_type: "image",
-        url: url,
-      }));
+      if (images.length > 0) {
+        const selectedImages = images.slice(0, 5);
+        const urlGallery = selectedImages.join(' ');
 
-      await sendMessage(
-        senderId,
-        {
-          attachment: {
-            type: "template",
-            payload: {
-              template_type: "media",
-              elements: attachments,
-            },
-          },
-        },
-        pageAccessToken
-      );
+        await sendMessage(senderId, { attachment: { type: 'image', payload: { url: urlGallery } } }, pageAccessToken);
+      } else {
+        await sendMessage(senderId, { text: 'No images found.' }, pageAccessToken);
+      }
     } catch (error) {
       await sendMessage(senderId, { text: 'Error: Could not generate image.' }, pageAccessToken);
     }
-  },
+  }
 };
