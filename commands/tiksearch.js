49e7be2dc9358f1fbb0793c0d5172fa9abcb580c
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2025-02-24 19:25:13 +0800

    Update tiksearch.js

--- commands/tiksearch.js
+++ commands/tiksearch.js
@@ -1,17 +1,12 @@
 const axios = require('axios');
 
-const headers = {
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-  'Content-Type': 'application/json'
-};
-
 module.exports = {
   name: 'tiksearch',
   description: 'Fetch a TikTok video based on a search term.',
   author: 'Cliff',
   
   async execute(senderId, args, pageAccessToken, sendMessage) {
-    const searchTerm = args.join(' ').trim();
+    const searchTerm = args.join(' ');
 
     if (!searchTerm) {
       return sendMessage(senderId, { text: '❌ Please provide a search' }, pageAccessToken);
@@ -20,7 +15,7 @@ module.exports = {
     const apiUrl = `https://betadash-search-download.vercel.app/tiksearch?search=${encodeURIComponent(searchTerm)}`;
 
     try {
-      sendMessage(senderId, { text: `🔍 Searching for '${searchTerm}', please wait...` }, pageAccessToken);
+      sendMessage(senderId, { text: `[ 🔍 ] Searching for: '${searchTerm}', please wait...` }, pageAccessToken);
 
       const response = await axios.get(apiUrl, { headers });
       const videoUrl = response.data.url;
