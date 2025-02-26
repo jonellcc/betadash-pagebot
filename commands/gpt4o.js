c0ddb616d2faf46454bcf538a6282dc2f3e28f31
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2025-02-26 10:47:27 +0800

    Update gpt4o.js

--- commands/gpt4o.js
+++ commands/gpt4o.js
@@ -15,12 +15,12 @@ module.exports = {
   async execute(senderId, args, pageAccessToken, sendMessage) {
     const prompt = args.join(' ');
 if (!prompt) {
-          sendMessage(senderId, { text: 'please provide a question first' }, pageAccessToken);
+         await sendMessage(senderId, { text: 'please provide a question first' }, pageAccessToken);
         return;
     }
 
     try {
-sendMessage(senderId, { text: '🔍 Searching Please Wait....' }, pageAccessToken);
+await sendMessage(senderId, { text: '🔍 Searching Please Wait....' }, pageAccessToken);
       const apiUrl = `https://yt-video-production.up.railway.app/gpt4-omni?ask=${encodeURIComponent(prompt)}&userid=${senderId}`;
       const response = await axios.get(apiUrl);
       const text = response.data.response;
@@ -29,13 +29,13 @@ sendMessage(senderId, { text: '🔍 Searching Please Wait....' }, pageAccessToke
       if (text.length > maxMessageLength) {
         const messages = splitMessageIntoChunks(text, maxMessageLength);
         for (const message of messages) {
-          sendMessage(senderId, { text: message }, pageAccessToken);
+          await sendMessage(senderId, { text: message }, pageAccessToken);
         }
       } else {
-        sendMessage(senderId, { text }, pageAccessToken);
+       await sendMessage(senderId, { text }, pageAccessToken);
       }
     } catch (error) {
-      sendMessage(senderId, { text: 'Sorry, there was an error processing your request.' }, pageAccessToken);
+      await sendMessage(senderId, { text: 'Sorry, there was an error processing your request.' }, pageAccessToken);
     }
   }
 };
