9c7f543e7e96d4e4b3582ac69a3121385ec63464
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2025-02-27 18:17:49 +0800

    Update horny.js

--- commands/horny.js
+++ commands/horny.js
@@ -6,6 +6,8 @@ module.exports = {
   author: 'Cliff',
   async execute(senderId, args, pageAccessToken, sendMessage, splitMessageIntoChunks) {
     const links = [
+"https://i.imgur.com/EIoKHsc.mp4",
+  "https://i.imgur.com/0HMrgWJ.mp4",
   "https://i.imgur.com/bbZsqXm.mp4",
  "https://i.imgur.com/uA4sNw7.mp4",
   "https://i.imgur.com/8mdshbv.mp4",
