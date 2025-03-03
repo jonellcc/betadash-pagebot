6a3e63b311c2675eac50256e4a5c7751a76dcf50
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2025-03-03 09:50:06 +0800

    Create copilot.js

---
+++ commands/copilot.js
@@ -0,0 +1,49 @@
+const as = require('axios');
+
+function s(m, c) {
+  const r = [];
+  for (let i = 0; i < m.length; i += c) {
+    r.push(m.slice(i, i + c));
+  }
+  return r;
+}
+
+module.exports = {
+  name: 'copilot',
+  description: 'github copilot model gpt4o',
+  author: 'yazky (rest api)',
+    async execute(senderId, args, pageAccessToken, sendMessage) {
+
+const s = senderId;
+const a = args;
+const p = pageAccessToken;
+const m = sendMessage;
+
+    const q = a.join(' ');
+
+    if (!q) {
+      await m(s, { text: 'Please provide a question first.' }, p);
+      return;
+    }
+
+    try {
+      const u = `https://betadash-api-swordslush.vercel.app/copilot?ask=${encodeURIComponent(q)}`;
+      const r = await as.get(u);
+      const t = r.data.response;
+
+      const l = 2000;
+      if (t.length > l) {
+        const c = s(t, l);
+        for (const x of c) {
+          const f = `󰟵 𝗚𝗶𝘁𝗵𝘂𝗯 𝗖𝗼𝗽𝗶𝗹𝗼𝘁\n━━━━━━━━━━━\n${x}\n━━━━━ ✕ ━━━━━`;
+         await m(s, { text: f }, p);
+        }
+      } else {
+        const f = `󰟵 𝗚𝗶𝘁𝗵𝘂𝗯 𝗖𝗼𝗽𝗶𝗹𝗼𝘁\n━━━━━━━━━━━━\n${t}\n━━━━━ ✕ ━━━━━`;
+       await m(s, { text: f }, p);
+      }
+    } catch (e) {
+      await m(s, { text: e.message }, p);
+    }
+  }
+};
