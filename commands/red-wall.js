c4b28a781e293a7a6fc19a6a04f416ba2abd4624
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2025-03-21 15:19:49 +0800

    Create red-wall.js

---
+++ commands/red-wall.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+const name = "red-wall";
+
+module.exports = {
+  name: name,
+  description: `${name} canvas`,
+  usage: `${name} <userid>`,
+  author: 'cliff',
+  async execute(senderId, args, pageAccessToken, sendMessage) {
+    if (!args || !Array.isArray(args) || args.length === 0) {
+      await sendMessage(senderId, { text: 'Please provide a userid to generate canvas' }, pageAccessToken);
+      return;
+    }
+
+    const uid = args.join(' ');
+
+    try {    
+      const apiUrl = `https://betadash-api-swordslush-production.up.railway.app/red-wall?userid=${uid}`;
+
+const nya = await axios.get(apiUrl);
+const res = nya.data.imageUrl;
+
+      await sendMessage(senderId, { attachment: { type: 'image', payload: { url: apiUrl } } }, pageAccessToken);
+
+    } catch (error) {
+      await sendMessage(senderId, { text: "This canvas is automatically face detected if the profile photo of user is no face detected can't generate canvas."}, pageAccessToken);
+    }
+  }
+};
