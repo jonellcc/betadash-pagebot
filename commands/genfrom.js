c604e3d5038be7e310c025d1429d19d9587e9e66
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2025-03-25 13:50:27 +0800

    Create genfrom.js

---
+++ commands/genfrom.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+
+module.exports = {
+  name: 'genfrom',
+  description: 'multi downloader GenfromDL',
+  author: 'yazky',
+  async execute(senderId, args, pageAccessToken, sendMessage) {
+    const apiUrl = `https://betadash-api-swordslush-production.up.railway.app/genfrom-dl?url=${encodeURIComponent(args.join(" "))}`;
+
+    try {
+      sendMessage(senderId, { text: `Downloading please wait...` }, pageAccessToken);
+
+      const response = await axios.get(apiUrl);
+      const data = response.data.data.data[0];
+      const title = data.title;
+      const thumbnail = data.thumbnail;
+      const videoUrl = data.links.find(link => link[1] === '240p' || link[1] === 'Link')[3];
+
+const ya = await axios.get(`https://betadash-api-swordslush.vercel.app/shorten?link=${videoUrl}`;
+
+const sht = ya.data.url;
+
+      sendMessage(senderId, {
+        attachment: {
+          "type": "template",
+          "payload": {
+            "template_type": "generic",
+            "elements": [
+              {
+                "title": title,
+                "image_url": thumbnail,
+                "subtitle": "",
+                "default_action": {
+                  "type": "web_url",
+                  "url": args.join(" "),
+                  "webview_height_ratio": "compact"
+                },
+                "buttons": [
+                  {
+                    "type": "web_url",
+                    "url": sht,
+                    "title": "Watch Video 🤫"
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      }, pageAccessToken);
+    } catch (error) {
+      sendMessage(senderId, { text: `[ ❌ ] Error fetching video data.` }, pageAccessToken);
+    }
+  }
+};
