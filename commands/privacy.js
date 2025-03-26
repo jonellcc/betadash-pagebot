8374dff43aca7152543ef30de351cbfdb63b0ed5
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2025-03-26 11:01:17 +0800

    Update privacy.js

--- commands/privacy.js
+++ commands/privacy.js
@@ -1,9 +1,16 @@
+const axios = require('axios');
+
 module.exports = {
   name: 'privacy',
   description: 'Rules for using the page bot',
   usage: 'privacy',
   author: 'cliff',
   async execute(senderId, args, pageAccessToken, sendMessage) {
+    const response = await axios.get(`https://graph.facebook.com/me?fields=id,name,picture.width(720).height(720).as(picture_large)&access_token=${pageAccessToken}`);
+    const profileUrl = response.data.picture_large.data.url;
+    const name = response.data.name;
+    const pageid = response.data.id;
+
     const termsAndConditions = `𝗧𝗘𝗥𝗠𝗦 𝗢𝗙 𝗦𝗘𝗥𝗩𝗜𝗖𝗘 & 𝗣𝗥𝗜𝗩𝗔𝗖𝗬 𝗣𝗢𝗟𝗜𝗖𝗬
 
 By using this bot, you agree to:
@@ -24,7 +31,7 @@ Failure to comply may result in access restrictions.`;
           buttons: [
             {
               type: "web_url",
-              url: "https://betadash-pagebot-production.up.railway.app/privacy",
+              url: `https://betadash-pagebot-production.up.railway.app/privacy/${pageid}`,
               title: "PRIVACY POLICY"
             }
           ]
