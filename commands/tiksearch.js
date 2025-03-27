70f31ab18246573d659990c1f4a669246219556d
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2025-03-27 19:23:46 +0800

    Update tiksearch.js

--- commands/tiksearch.js
+++ commands/tiksearch.js
@@ -12,13 +12,7 @@ module.exports = {
       const response = await axios.get(apiUrl);
       const videos = response.data.data;
 
-      const quickReplies = videos.map((video, index) => ({
-        content_type: "text",
-        title: `Watch ${index + 1}`,
-        payload: `WATCH_VIDEO_${index}`,
-      }));
-
-      const elements = videos.map((video) => ({
+      const elements = videos.map((video, index) => ({
         title: video.title,
         image_url: video.cover,
         default_action: {
@@ -30,51 +24,42 @@ module.exports = {
           {
             type: 'web_url',
             url: video.video,
-            title: 'Download'
+            title: 'Watch Video'
           }
+      /**    {
+            type: 'postback',
+            title: `Watch ${index + 1}`,
+            payload: `WATCH_VIDEO_${index}`
+          } **/
         ]
       }));
 
-      await sendMessage(senderId, {
+      const message = {
         attachment: {
           type: 'template',
           payload: {
             template_type: 'generic',
             elements: elements
           }
-        },
-        quick_replies: quickReplies
-      }, pageAccessToken);
-    } catch (error) {
-      await sendMessage(senderId, { text: "Failed to fetch TikTok videos." }, pageAccessToken);
-    }
-  },
-
-  async handleQuickReply(senderId, payload, pageAccessToken, sendMessage, searchQuery) {
-    if (payload.startsWith("WATCH_VIDEO_")) {
-      const videoIndex = parseInt(payload.replace("WATCH_VIDEO_", ""), 10);
-      const apiUrl = `https://betadash-api-swordslush-production.up.railway.app/tiksearchv2?search=${encodeURIComponent(searchQuery)}&count=10`;
-
-      try {
-        const response = await axios.get(apiUrl);
-        const videos = response.data.data;
-
-        if (videoIndex >= 0 && videoIndex < videos.length) {
-          const videoUrl = videos[videoIndex].video;
-
-          await sendMessage(senderId, {
-            attachment: {
-              type: 'video',
-              payload: {
-                url: videoUrl,
-                is_reusable: true
-              }
-            }
-          }, pageAccessToken);
         }
-      } catch (error) {
-        await sendMessage(senderId, { text: "Failed to retrieve the video." }, pageAccessToken);
-      }
+      };
+
+      await sendMessage(senderId, message, pageAccessToken);
+    } catch (error) {
+      await sendMessage(senderId, {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "media",
+            elements: [
+              {
+                media_type: "video",
+                url: "https://www.facebook.com/beluga.xyz/videos/2070790143388193/?app=fbl"
+              }
+            ]
+          }
+        }
+      }, pageAccessToken);
     }
   }
 };
